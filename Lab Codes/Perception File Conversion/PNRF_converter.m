fb4f24f19837cd0825a3f03ac141d6187733ff43
Djlawson1000 <djlawson1000@gmail.com>
2025-03-19 16:16:34 -0400

    Updates

--- Lab Codes/Perception File Conversion/PNRF_converter.m
+++ Lab Codes/Perception File Conversion/PNRF_converter.m
@@ -31,7 +31,7 @@
 % all the files counted for that handle in sequential order from 1--999. 
 clc; clear; close all
 
-blocks={'Test057'};%,'kcal','dcal','aftertest'
+blocks={'Pitot5'};%,'kcal','dcal','aftertest'
 pnrf_converter(blocks);
  
 function []=pnrf_converter(blocks)
